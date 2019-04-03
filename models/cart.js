34d62c5bdf1c280f5f0ac10a3e46764eb0776218
eoghanlawless <home.eoghanlawless@gmail.com>
2019-04-03 22:07:03 +0100

    Removed unneeded commented code

--- models/cart.js
+++ models/cart.js
@@ -1,18 +1,3 @@
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-
-// var schema = new Schema({
-//     title: {type: String, required: true},
-//     description: {type: String, required: true},
-//     price: {type: Number, required: true},
-//     thumbnail: {type: String, required: true},
-
-//     brand: {type: String, required: true},
-//     category: {type: String, required: true}
-// });
-
-// module.exports = mongoose.model('Cart', schema);
-
 module.exports = function Cart(Cart) {
     this.products = Cart.products || {};
     this.quantity = Cart.quantity || 0;
